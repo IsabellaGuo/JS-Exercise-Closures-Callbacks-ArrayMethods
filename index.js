cc83437f8dec555da3d4a2eebddb3cb05c12ae7f
IsabellaGuo <isabella.guofei@gmail.com>
2019-11-12 13:28:38 -0800

    third done

--- index.js
+++ index.js
@@ -89,8 +89,12 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
+  return callback (numberList.reduce(function(accumulator, numberList){
+    return accumulator + numberList;
+  },0);
+  )
 }
 
 /**
